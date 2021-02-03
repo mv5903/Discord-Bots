4776d0f95bf5bccdabd5cda84a3e7c6f2f2d9370
Matthew Vandenberg <mv5903@gmail.com>
2021-02-03 10:57:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,6 @@ client.on('guildMemberRemove', member => {
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-	
 	let voiceChannels = newMember.guild.channels.cache.forEach((channel) => {
 		if (channel.parentID === '806506130737463309') {
 			if (channel.members.size == 0) {
