498ae43ddbb6d92f0c2644c26b353332ad22f3f6
Matthew Vandenberg <mv5903@gmail.com>
2021-02-03 11:09:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,6 @@ function allowIntoVC(member, isAdding) {
 }
 
 client.on('message', message => {
-	
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).split(" ");
 	const command = args.shift().toLowerCase();
