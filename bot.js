bdc5c6d18529d4031d85a492d7617fc55c6c4e1c
Matthew Vandenberg <mv5903@gmail.com>
2021-03-17 16:27:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -585,7 +585,7 @@ async function mc() {
 			.setTitle('Minecraft Server')
 			.setThumbnail("https://store-images.s-microsoft.com/image/apps.45782.9007199266731945.debbc4f1-cde0-491b-8c6f-b6b015eecab6.4716cccc-5f37-4bb5-9db1-0c1dbc99003f?mode=scale&q=90&h=200&w=200&background=%23000000")
 			.addFields(
-				{name: 'Server', value: website},
+				{name: 'Server', value: 'kwikmatt.ddns.net'},
 				{name: 'Version', value: version},
 				{name: 'Status', value: state},
 				{name: 'Players Online', value: data.players.online + '/' + data.players.max},
